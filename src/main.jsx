9e899ca708c98728c327ceb5adef2010d7661467
Ashutosh Swamy <ashutoshswamy397@gmail.com>
2026-06-13 18:19:15 +0530

    updated ms clarity code 🚀

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, useLocation } from 'react-router-dom'
 import { inject } from '@vercel/analytics'
 import { injectSpeedInsights } from '@vercel/speed-insights'
+import Clarity from '@microsoft/clarity'
 import './index.css'
 import App from './App.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 
 inject()
 injectSpeedInsights()
+Clarity.init('x6dr3p2nzv')
 
 function ScrollToTop() {
   const { pathname } = useLocation();
